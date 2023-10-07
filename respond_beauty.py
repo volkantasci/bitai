119270658ed35970587d36145327d6b4e4b7a99f
Volkan TAŞCI <me@volkantasci.com>
2023-10-07 05:14:22 +0300

    Chat PDFs added

--- respond_beauty.py
+++ respond_beauty.py
@@ -1,4 +1,9 @@
 def make_it_beautiful(respond: str):
+    """
+    If you use custom HTML template in your responses, you will need to use this function.
+    Otherwise, you will get a lot of problems with your HTML code.
+
+    """
     respond = respond.strip()
     respond = respond.replace('\n', '<br>')
     s = respond.split('```')
